6faac32f922abaf8cec6167b21a3cdc977a996b5
ioaznnis <1431061+ioaznnis@users.noreply.github.com>
2019-10-29 00:22:58 +0300

    Source Code

---
+++ DeviceManager/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace DeviceManager
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await UsbDeviceInsertWatcher(TimeSpan.FromMinutes(1));
+        }
+
+        /// <summary>
+        /// Watcher for new usb Device
+        /// </summary>
+        /// <param name="delay">Time to register connected device</param>
+        /// <returns></returns>
+        private static async Task UsbDeviceInsertWatcher(TimeSpan delay)
+        {
+            // Create event query to be notified within 3 second of 
+            // a new USB device inserted
+            var query = new WqlEventQuery("__InstanceCreationEvent",
+                TimeSpan.FromSeconds(3),
+                "TargetInstance ISA 'Win32_USBHub'");
+
+            // Initialize an event watcher and subscribe to events 
+            // that match this query
+            using (var insertWatcher = new ManagementEventWatcher(query))
+            {
+                insertWatcher.EventArrived += DeviceInsertedEvent;
+                insertWatcher.Start();
+                //Wait for connected device
+                await Task.Delay(delay);
+                insertWatcher.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Отображение информации о подключенном device
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            using (var managementBaseObject = (ManagementBaseObject) e.NewEvent["TargetInstance"])
+            {
+                foreach (var property in managementBaseObject.Properties)
+                {
+                    Console.WriteLine($"{property.Name} {property.Value}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SampleDrmApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Management;
+
+namespace SampleDrmApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert key.");
+            Console.WriteLine(@"Example: USB\\VID_***&PID_***\\***");
+
+            var key = Console.ReadLine();
+
+            using var searcher = new ManagementObjectSearcher($"Select * From Win32_USBHub where DeviceID='{key}'");
+            using var deviceCollection = searcher.Get();
+            Console.WriteLine(deviceCollection.Count == 0 ? "Wrong key!" : "Key accept!");
+
+            // You DRM data here...
+
+            Console.ReadLine();
+        }
+    }
+}
